6eccc77e1230ec443fa59a5745afe01e94e8a0a4
vedant756 <vedantseth@outlook.com>
2023-06-25 01:07:32 +0530

    feat: Add dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Resturant from "./component/basics/Resturant";
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
 
-const App = () => {
-  return <Resturant />;
-};
+function App() {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className="App" data-theme={darkMode ? 'dark' : 'light'}>
+      {<Resturant /> }
+      <button onClick={toggleDarkMode}>
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
+    </div>
+  );
+}
 export default App;
